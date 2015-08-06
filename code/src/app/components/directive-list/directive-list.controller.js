5c094cfde0591a51437599f65ceb858d71292311
Jon DeJong <jon.dejong@gmail.com>
2015-08-05 21:43:21 -0500

    listing

--- code/src/app/components/directive-list/directive-list.controller.js
+++ code/src/app/components/directive-list/directive-list.controller.js
@@ -1,23 +1,43 @@
-(function() {
-    'use strict';
+(function () {
+  'use strict';
 
-    angular
-        .module('playground')
-        .controller('DirectiveListController', DirectiveListController);
+  angular
+    .module('playground')
+    .controller('DirectiveListController', DirectiveListController);
 
-    /** @ngInject */
-    function DirectiveListController() {
-        var self = this;
+  /** @ngInject */
+  function DirectiveListController() {
+    var self = this;
 
-        console.log("in the DirectiveListController controller");
+    console.log("in the DirectiveListController controller");
 
-        self.directives = [
-            {
-                name: "Hello World",
-                description: "The simplest directive. Put stuff on the page.",
-                url: "/"
-            }
-        ];
+    self.directives = [
+      {
+        name: "Hello World",
+        description: "The simplest directive. Put stuff on the page.",
+        url: "/"
+      },
+      {
+        name: "Other 1",
+        description: "The simplest directive. Put stuff on the page.",
+        url: "/"
+      },
+      {
+        name: "Other 2",
+        description: "The simplest directive. Put stuff on the page.",
+        url: "/"
+      },
+      {
+        name: "Other 3",
+        description: "The simplest directive. Put stuff on the page.",
+        url: "/"
+      },
+      {
+        name: "Other 4",
+        description: "The simplest directive. Put stuff on the page.",
+        url: "/"
+      }
+    ];
 
-    }
+  }
 })();
