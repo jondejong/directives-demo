648c77c9d93429e7a02ebaccd3d47eccada2ba14
Jon DeJong <jon.dejong@gmail.com>
2015-08-09 15:06:49 -0500

    just backup up

--- code/src/app/components/dog/dogList.directive.spec.js
+++ code/src/app/components/dog/dogList.directive.spec.js
@@ -3,7 +3,9 @@
 
       describe('directive:dogList', function () {
 
-        var $compile, $rootScope, $q, element, DogService, mockPromise;
+        var $compile, $rootScope, $q, element,
+          DogService, mockPromise, dogs,
+          firstDiv, secondDiv;
 
         beforeEach(module('playground'));
 
@@ -17,20 +19,25 @@
             DogService.getDogs = function () {
                 return mockPromise;
             };
+
+            dogs = [
+              {name: "Bear", age: "3", breed: "Collie"},
+              {name: "Snoop", age: "4", breed: "Puggle"}
+            ]
         }));
 
         var parseDirective = function() {
           var scope = $rootScope.$new();
           element = $compile("<dog-list></dog-list>")(scope);
           scope.$digest();
+          var divs = element.find('div');
+          firstDiv = angular.element(divs[0]);
+          secondDiv = angular.element(divs[1]);
         };
 
         it('Sets up a loading screen', function () {
             parseDirective();
 
-            var divs = element.find('div');
-            var firstDiv = angular.element(divs[0]);
-            var secondDiv = angular.element(divs[1]);
             expect(firstDiv.hasClass('ng-hide')).toBeFalsy();
             expect(secondDiv.hasClass('ng-hide')).toBeTruthy();
         });
@@ -39,6 +46,21 @@
             spyOn(DogService, 'getDogs').and.returnValue(mockPromise);
             parseDirective();
             expect(DogService.getDogs).toHaveBeenCalled();
+        });
+
+        it('Shows the dogs', function () {
+          var defer = $q.defer();
+          DogService.getDogs = function () {
+            return defer.promise;
+          };
+
+          parseDirective();
+
+          defer.resolve(dogs);
+          $rootScope.$apply();
+
+          expect(firstDiv.hasClass('ng-hide')).toBeTruthy();
+          expect(secondDiv.hasClass('ng-hide')).toBeFalsy();
         });
 
     });
