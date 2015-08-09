e6c2489ab8a06f9e47f1118590479308d0d42f8a
Jon DeJong <jon.dejong@gmail.com>
2015-08-09 09:32:29 -0500

    just backup up

---
+++ code/src/app/components/helloAttr/helloAttr.directive.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('playground')
+        .directive('helloPerson', function () {
+            return {
+                restrict: 'E',
+                templateUrl: 'app/components/helloPerson/helloPerson.template.html',
+                link: function (scope, el, attrs) {
+                    scope.name = attrs.name;
+                }
+            };
+        });
+})();
---
+++ code/src/app/components/helloAttr/helloAttr.directive.spec.js
@@ -0,0 +1,24 @@
+(function () {
+    'use strict';
+
+    describe('directive:helloPerson', function () {
+
+        var $compile, $rootScope;
+
+        beforeEach(module('playground'));
+
+        beforeEach(inject(function (_$compile_, _$rootScope_) {
+            $compile = _$compile_;
+            $rootScope = _$rootScope_;
+        }));
+
+
+        it('Replaces the HTML content correctly', function () {
+            var html = '<hello-person name="Jonny"></hello-person>';
+            var element = $compile(html)($rootScope);
+            $rootScope.$digest();
+            expect(element.html()).toContain("Hello Jonny!!!");
+        });
+
+    });
+})();
