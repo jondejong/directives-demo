ccce7cc11feda634c1f7d1448383cb031d6dc2a7
Jon DeJong <jon.dejong@gmail.com>
2015-08-09 22:07:37 -0500

    just backup up

--- code/src/app/components/dogObject/dogObject.directive.js
+++ code/src/app/components/dogObject/dogObject.directive.js
@@ -11,8 +11,6 @@
         restrict: 'E',
         templateUrl: 'app/components/dogObject/dogObject.template.html',
         link: function (scope, el, attrs) {
-          console.log("dog object with scope", scope)
-          console.log("dog object with data", scope.data)
         }
       };
     });
--- code/src/app/components/dogObject/dogObject.directive.spec.js
+++ code/src/app/components/dogObject/dogObject.directive.spec.js
@@ -1,75 +1,48 @@
 (function () {
-    'use strict';
+  'use strict';
 
-      describe('directive:dogList', function () {
+  fdescribe('directive:dogObject', function () {
 
-        var $compile, $rootScope, $q, element,
-          DogService, mockPromise, dogs,
-          firstDiv, secondDiv;
+    var $compile, $rootScope, element, scope, dogs,
+      div;
 
-        beforeEach(module('playground'));
+    beforeEach(module('playground'));
 
-        beforeEach(inject(function (_$compile_, _$rootScope_, _DogService_, _$q_) {
-            $compile = _$compile_;
-            $rootScope = _$rootScope_;
-            DogService = _DogService_;
-            $q = _$q_;
+    beforeEach(inject(function (_$compile_, _$rootScope_) {
+      $compile = _$compile_;
+      $rootScope = _$rootScope_;
+      scope = $rootScope.$new();
+      scope.data = {};
 
-            mockPromise = {then: function() {}}
-            DogService.getDogs = function () {
-                return mockPromise;
-            };
+      dogs = [
+        {name: "Bear", age: "3", breed: "Collie"},
+        {name: "Snoop", age: "4", breed: "Puggle"}
+      ]
+    }));
 
-            dogs = [
-              {name: "Bear", age: "3", breed: "Collie"},
-              {name: "Snoop", age: "4", breed: "Puggle"}
-            ]
-        }));
+    var parseDirective = function () {
+      element = $compile("<dog-object data='data'></dog-object>")(scope);
+      scope.$digest();
+      div = angular.element(element.find('div'));
+    };
 
-        var parseDirective = function() {
-          var scope = $rootScope.$new();
-          element = $compile("<dog-list></dog-list>")(scope);
-          scope.$digest();
-          var divs = element.find('div');
-          firstDiv = angular.element(divs[0]);
-          secondDiv = angular.element(divs[1]);
-        };
+    var isHidden = function (el) {
+      return el.hasClass('ng-hide');
+    };
 
-        var isHidden = function(el) {
-          return el.hasClass('ng-hide');
-        };
-
-        it('Sets up a loading screen', function () {
-            parseDirective();
-
-            expect(isHidden(firstDiv)).toBeFalsy();
-            expect(isHidden(secondDiv)).toBeTruthy();
-        });
-
-        it('Asks the service for dogs', function () {
-            spyOn(DogService, 'getDogs').and.returnValue(mockPromise);
-            parseDirective();
-            expect(DogService.getDogs).toHaveBeenCalled();
-        });
-
-        it('Shows the dogs', function () {
-          var defer = $q.defer();
-          DogService.getDogs = function () {
-            return defer.promise;
-          };
-
-          parseDirective();
-
-          defer.resolve(dogs);
-          $rootScope.$apply();
-
-          expect(isHidden(firstDiv)).toBeTruthy();
-          expect(isHidden(secondDiv)).toBeFalsy();
-
-          // We should have added two tiles
-          var tiles = secondDiv.find('md-grid-tile');
-          expect(tiles.length).toBe(2);
-        });
-
+    it('Starts out empty', function () {
+      parseDirective();
+      expect(isHidden(div)).toBeTruthy();
     });
+
+    fit('List the dogs when the populate', function () {
+      parseDirective();
+      scope.data.dogs = dogs;
+      scope.$digest();
+      expect(isHidden(div)).toBeFalsy();
+      var tiles = div.find('md-grid-tile');
+      expect(tiles.length).toBe(2);
+    });
+
+  });
 })();
