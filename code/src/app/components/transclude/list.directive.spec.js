aa8d93c3b83049a17ff492fd2bd9f2e71d36b9b7
Jon DeJong <jon.dejong@gmail.com>
2015-08-12 14:05:11 -0500

    just backing up the things

--- code/src/app/components/transclude/list.directive.spec.js
+++ code/src/app/components/transclude/list.directive.spec.js
@@ -3,34 +3,35 @@
 
   describe('directive:list', function () {
 
-    var $compile, $rootScope;
+    var $compile, $rootScope, element;
 
     beforeEach(module('playground'));
 
     beforeEach(inject(function (_$compile_, _$rootScope_) {
       $compile = _$compile_;
       $rootScope = _$rootScope_;
-    }));
 
-
-    it('Replaces the HTML content correctly', function () {
       var html = '<list things="bots" type-name="Bot"></list>';
       var scope = $rootScope.$new();
       scope.bots = [
         {name: 'bot 1', description: 'awesome'},
         {name: 'bot 2', description: 'ok'}
       ];
-      var element = $compile(html)(scope);
+      element = $compile(html)(scope);
       scope.$digest();
 
-      // It should add the outer div
+    }));
+
+
+    it('Should add the outer div', function () {
       var div = angular.element(element.find('div'));
       expect(div.hasClass('list-data')).toBeTruthy();
+    });
 
-      // It should create a thing for each "bot"
+    it('Should create a thing for each bot', function () {
+      var div = angular.element(element.find('div'));
       var things = div.find('thing');
       expect(things.length).toBe(2);
-
     });
 
   });
